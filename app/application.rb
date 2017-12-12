0e032e0120fd3a83a676930d5b9a47e9e0fefc06
Eric McKinney <mckinney99@gmail.com>
2017-12-12 03:09:04 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,13 +4,18 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path=="/items/<ITEM NAME>"
-      resp.write "You requested the songs"
-    else
+    if req.path.match(/items/)
+     item_name = req.path.split("/items/").last
+   if item = @@items.find{|i| i.name == item_name}
+     resp.write item.price
+  else
+    resp.write "Item not found"
+    resp.status = 400
+  end
+  else
       resp.write "Route not found"
-      resp.status = 404
+      resp.status=404
     end
-
-    resp.finish
+  resp.finish
   end
 end
